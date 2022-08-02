0ebbd6f40183d9f4800727be1c4b8642725cbceb
lucasz10 <lucas.zach10@gmail.com>
2022-08-02 16:38:54 -0700

    Created functions for search history buttons and retrieving geographical information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,62 @@
+//Declaring global variables
 var searchBTN = document.getElementById('searchBtn');
 var apiKey = "458caba92592b3c1eece46e7c6fef991";
+
+var cityInput;
 
 var cityReport = $('#cityReport');
 var searchHistory = $('#searchHistory');
 
 var recentSearches = JSON.parse(localStorage.getItem('recentSearches')) || [];
 
-searchBTN.addEventListener("click", formSearch);
+//Declaring functions
+function formSearch() { //takes search input and gathers necessary information
 
-function formSearch() {
-    var cityInput = document.getElementById('cityInput').value;
+    cityInput = document.getElementById('cityInput').value;
 
     if(!recentSearches.includes(cityInput)) {
-        recentSearches.push(cityInput);
+        recentSearches.unshift(cityInput);
     }
     localStorage.setItem('recentSearches', JSON.stringify(recentSearches))
+
+    $('#cityInput').val(""); //Removes text from input field after clicking search
+    searchButtonCreate(); //Creates a button from previous entered text
+
+    //Retrieves the latitude and longitude of the cityInput
+    var geocodeCall = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${apiKey}`
+
+    fetch(geocodeCall)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        var latitude = data[0].lat;
+        var longitude = data[0].lon;
+
+        console.log(latitude);
+        console.log(longitude);
+      });
+
+
     
 };
+
+function searchButtonCreate() { //Creates history buttons based on local storage
+
+    $('#searchHistory').empty(); //Clears history before reinserting
+     
+    for(let i = 0; i < recentSearches.length; i++) {
+        $('#searchHistory').append('<a type="submit" class="btn btn-primary w-100 text-white my-2" id="historyBtn">' + recentSearches[i] + "</a>");
+    }
+};
+
+
+
+
+
+//Adding event listeners
+searchBTN.addEventListener("click", formSearch);
+
+//Calling certain functions on page load
+searchButtonCreate();
