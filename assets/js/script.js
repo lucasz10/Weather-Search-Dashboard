5e81a10f3ce738fe85873da2d5601a45027ee47b
lucasz10 <lucas.zach10@gmail.com>
2022-08-02 18:33:25 -0700

    Created function to change display of information. Still need to change 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,9 @@ var searchHistory = $('#searchHistory');
 
 var recentSearches = JSON.parse(localStorage.getItem('recentSearches')) || [];
 
+var latitude;
+var longitude;
+
 //Declaring functions
 function formSearch() { //takes search input and gathers necessary information
 
@@ -22,25 +25,36 @@ function formSearch() { //takes search input and gathers necessary information
     $('#cityInput').val(""); //Removes text from input field after clicking search
     searchButtonCreate(); //Creates a button from previous entered text
 
-    //Retrieves the latitude and longitude of the cityInput
+    //Retrieves the latitude and longitude of the cityInput, only takes the first result
     var geocodeCall = `http://api.openweathermap.org/geo/1.0/direct?q=${cityInput}&limit=1&appid=${apiKey}`
 
     fetch(geocodeCall)
     .then(function (response) {
+        
         return response.json();
     })
     .then(function (data) {
-        console.log(data);
-        var latitude = data[0].lat;
-        var longitude = data[0].lon;
+       
+        latitude = data[0].lat;
+        longitude = data[0].lon;
+        var weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely,alerts&units=imperial&appid=${apiKey}`
 
+
+        fetch(weatherURL) //Gets data based on city information
+        .then(function (response) {
+            return response.json();
+        })
+        .then (function (data) {
+            displayWeather(data)
+        });
         console.log(latitude);
-        console.log(longitude);
-      });
+    })
 
+    console.log(latitude);
 
     
 };
+
 
 function searchButtonCreate() { //Creates history buttons based on local storage
 
@@ -51,7 +65,29 @@ function searchButtonCreate() { //Creates history buttons based on local storage
     }
 };
 
+function displayWeather(weatherInfo) {
+    console.log(weatherInfo);
 
+    $('#currentCity').text(cityInput + " , " + weatherInfo.current.dt)
+    
+    $('#currentTemp').text("Temp: " + weatherInfo.current.temp);
+    $('#currentIcon').setAtt
+    $('#currentWind').text("Wind Speed: " + weatherInfo.current.wind_speed);
+    $('#currentHumidity').text("Humidity: " + weatherInfo.current.humidity + "%");
+    $('#currentUV').text("UV Index: " + weatherInfo.current.uvi);
+
+    //Adjusts UV Index Background based on how high it is
+    if (weatherInfo.current.uvi <= 2.9) {
+        $('#currentUV').removeClass("bg-warning bg-danger").addClass("bg-success");
+      } else if (weatherInfo.current.uvi > 2.9 && weatherInfo.current.uvi <= 5.9) {
+        $('#currentUV').removeClass("bg-success bg-danger").addClass("bg-warning");
+      } else {
+        $('#currentUV').removeClass("bg-warning bg-success").addClass("bg-danger");
+      }
+
+
+      $('#5DayForecast').empty(); //Removes previous city data to create new elements
+}
 
 
 
